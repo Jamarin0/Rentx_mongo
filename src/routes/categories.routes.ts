046f19c4e848bf96b117e1d58fe0efaddb35e665
Jamarin0 <jgjsguilherme@gmail.com>
2021-07-21 01:52:27 -0300

    fix: refatorando import e specification

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,22 +2,21 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/car/useCases/createCategory/CreateCategoryController";
-import { importCategoryController } from "../modules/car/useCases/importCategory";
-import { listCategoriesController } from "../modules/car/useCases/listCategories.ts";
+import { ImportCategoryController } from "../modules/car/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/car/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoutes = Router();
 
 const upload = multer({ dest: "./tmp" });
 
 const createCategoryController = new CreateCategoryController();
+const listCategoriesController = new ListCategoriesController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
-categoriesRoutes.get("/", (request, response) => {
-    return listCategoriesController.handle(request, response);
-});
+categoriesRoutes.get("/", listCategoriesController.handle)
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    return importCategoryController.handle(request, response);
-});
+categoriesRoutes.post("/import", upload.single("file"), importCategoryController.handle);
+
 export { categoriesRoutes };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from "tsyringe";
 
 import {ICategoriesRepository} from "../../modules/car/repositories/ICategoriesRepository";
 import {CategoriesRepository} from "../../modules/car/repositories/implementations/CategoriesRepository";
-// ICategoryRepository
+import { SpecificationsRepository } from "../../modules/car/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/car/repositories/ISpecificationsRepository";
+
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+    "SpecificationsRepository",
+    SpecificationsRepository
 );
