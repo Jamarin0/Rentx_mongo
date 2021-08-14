092cf401ae404ba4dcb7b5fc109a9f3c931dea18
Jamarin0 <jgjsguilherme@gmail.com>
2021-08-13 23:20:53 -0300

    feat: add authenticate user

--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,17 +1,27 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 @injectable()
 class CreateUserUseCase {
     constructor(
-    @inject("UsersRepository")
-    private usersRepository: IUsersRepository
-    ){}
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) { }
 
-    async execute({name, username, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+    async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExists){
+            throw new Error("User already exists");            
+        }
+        
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
-            name, username, email, password, driver_license
+            name, email, password: passwordHash, driver_license
         });
     }
 }
