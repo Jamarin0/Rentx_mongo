89693814a5975e660ef12a6a8d5e5539ea389626
Jamarin0 <jgjsguilherme@gmail.com>
2021-08-19 01:07:50 -0300

    fix: user avatar

---
+++ src/config/upload.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+import { resolve } from "path";
+
+export default {
+    
+    upload(folder: string) {
+        return {
+            storage: multer.diskStorage({
+                
+            })
+        }
+    }
+}
--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -23,9 +23,13 @@ export async function ensureAuthenticated(request: Request, response: Response, 
         const usersRepository = new UsersRepository();
         const user = await usersRepository.findById(user_id);
 
-        if(!user) {
+        if (!user) {
             throw new AppError("User does not exists!", 401);
         }
+
+        request.user = {
+            id: user_id,
+        };
 
         next();
     } catch {
